a2e93250979da626ccc6cbc1c2b97d590a02bbf9
user180010 <user180010@74-80-245-22.ic2net.net>
2020-09-22 18:41:58 -0700

    Video 1 Complete

---
+++ Make It So SL Live/Make It So SL Live/Make_It_So_SL_LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_It_So_SL_LiveApp.swift
+//  Make It So SL Live
+//
+//  Created by user180010 on 9/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Make_It_So_SL_LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TaskListView()
+        }
+    }
+}
