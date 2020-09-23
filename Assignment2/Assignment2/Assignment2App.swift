5d702077a91d4dd6ebbe8447739164eab370ef3e
user180010 <user180010@74-80-245-22.ic2net.net>
2020-09-22 19:23:40 -0700

    add Assignment2

---
+++ Assignment2/Assignment2/Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by user180010 on 9/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            TaskListView()
+        }
+    }
+}
---
+++ Assignment2/Assignment2/ViewModels/TaskListViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  TaskListViewModel.swift
+//  Make It So SL Live 2
+//
+//  Created by user180010 on 9/21/20.
+//
+import Foundation
+import Combine
+
+class TaskListViewModel: ObservableObject{
+    @Published var taskCellViewModels = [TaskCellViewModel]()
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        self.taskCellViewModels = testDataTasks.map { task in
+            TaskCellViewModel(task: task)
+        }
+    }
+    
+    func addTask(task:Task) {
+        let taskVM = TaskCellViewModel(task: task)
+        self.taskCellViewModels.append(taskVM)
+    }
+}
